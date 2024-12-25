b149942d6954cda8199f22a5c09a94aeb2c1bc5e
Shayan Codes <shayanjameel76@gmail.com>
2024-12-25 15:10:58 -0800

    second commit

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+#print("Hello, World!")
+
+character_name = "Max"
+
+'''print( character_name  + " wanted to have a chocolate. ")
+print("Johns\nAcademy")'''
+
+word = "Hello World!"
+#print(word.lower().islower())
+#print(word.lower().isupper())
+print(len(word))
+print(word.index("W"))
